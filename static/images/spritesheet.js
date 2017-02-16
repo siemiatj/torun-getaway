582116ca2b1a895dc0782c3ae2ac4f0a6a829478
siemiatj <kuba@saucelabs.com>
2017-02-16 03:10:59 +0100

    - adding sprites

--- static/images/spritesheet.js
+++ static/images/spritesheet.js
@@ -7,31 +7,35 @@ const SPRITES = {
   
     TRUCK: { x: 0, y: 144, w: 100, h: 78 },
   
-    MISIEWICZ: { x: 250, y: 0, w: 80, h: 80 },
+    MONISTER: { x: 250, y: 0, w: 80, h: 80 },
   
-    MACIARENKO: { x: 250, y: 80, w: 80, h: 80 },
+    APTEKARZ: { x: 250, y: 80, w: 80, h: 80 },
   
-    BEATKA: { x: 0, y: 222, w: 80, h: 80 },
+    CIOCIA: { x: 0, y: 222, w: 80, h: 80 },
   
     CAR03: { x: 100, y: 144, w: 88, h: 55 },
   
     CAR02: { x: 250, y: 160, w: 80, h: 59 },
   
-    CAR04: { x: 80, y: 222, w: 80, h: 57 },
+    MALUCH: { x: 80, y: 222, w: 65, h: 71 },
   
-    CAR01: { x: 160, y: 222, w: 80, h: 56 },
+    CAR04: { x: 145, y: 222, w: 80, h: 57 },
   
-    PLAYER_UPHILL_LEFT: { x: 240, y: 222, w: 80, h: 45 },
+    CAR01: { x: 225, y: 222, w: 80, h: 56 },
   
-    PLAYER_UPHILL_RIGHT: { x: 330, y: 0, w: 80, h: 45 },
+    SEICENTO: { x: 330, y: 0, w: 77, h: 56 },
   
-    PLAYER_UPHILL_STRAIGHT: { x: 330, y: 45, w: 80, h: 45 },
+    PLAYER_UPHILL_LEFT: { x: 0, y: 302, w: 80, h: 45 },
   
-    PLAYER_RIGHT: { x: 330, y: 90, w: 80, h: 41 },
+    PLAYER_UPHILL_RIGHT: { x: 80, y: 302, w: 80, h: 45 },
   
-    PLAYER_STRAIGHT: { x: 330, y: 131, w: 80, h: 41 },
+    PLAYER_UPHILL_STRAIGHT: { x: 160, y: 302, w: 80, h: 45 },
   
-    PLAYER_LEFT: { x: 330, y: 172, w: 80, h: 41 },
+    PLAYER_STRAIGHT: { x: 240, y: 302, w: 80, h: 41 },
+  
+    PLAYER_RIGHT: { x: 320, y: 302, w: 80, h: 41 },
+  
+    PLAYER_LEFT: { x: 0, y: 347, w: 80, h: 41 },
   
 }
 
