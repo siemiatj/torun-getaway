ad69ddb318807898f3cdb07dce5059cc7c1873bd
siemiatj <kuba@saucelabs.com>
2017-02-17 21:11:24 -0700

    - more cleanup in sprites

--- static/images/spritesheet.js
+++ static/images/spritesheet.js
@@ -1,49 +1,59 @@
 
 const SPRITES = {
   
-    SEMI: { x: 0, y: 0, w: 122, h: 144 },
+    TREE: { x: 0.5, y: 0.5, w: 226, h: 200 },
   
-    ICONFB: { x: 122, y: 0, w: 128, h: 128 },
+    ICONFB: { x: 227.5, y: 0.5, w: 128, h: 128 },
   
-    SEMI2: { x: 250, y: 0, w: 120, h: 114 },
+    TIR: { x: 0.5, y: 201.5, w: 120, h: 114 },
   
-    TRUCK: { x: 0, y: 144, w: 100, h: 78 },
+    TRUCK: { x: 121.5, y: 201.5, w: 98, h: 112 },
   
-    MONISTER: { x: 0, y: 222, w: 80, h: 80 },
+    VAN: { x: 220.5, y: 201.5, w: 86, h: 84 },
   
-    CIOCIA: { x: 80, y: 222, w: 80, h: 80 },
+    MONISTER: { x: 356.5, y: 0.5, w: 80, h: 80 },
   
-    APTEKARZ: { x: 160, y: 222, w: 80, h: 80 },
+    APTEKARZ: { x: 356.5, y: 81.5, w: 80, h: 80 },
   
-    MONDEO: { x: 100, y: 144, w: 77, h: 65 },
+    CIOCIA: { x: 356.5, y: 162.5, w: 80, h: 80 },
   
-    CAR03: { x: 177, y: 144, w: 88, h: 55 },
+    PLAYER_1_LEFT: { x: 356.5, y: 243.5, w: 78, h: 66 },
   
-    CAR02: { x: 240, y: 222, w: 80, h: 59 },
+    PLAYER_1_RIGHT: { x: 227.5, y: 129.5, w: 78, h: 66 },
   
-    POLDEK1: { x: 0, y: 302, w: 80, h: 59 },
+    MONDEO: { x: 0.5, y: 316.5, w: 77, h: 65 },
   
-    CAR04: { x: 80, y: 302, w: 80, h: 57 },
+    PLAYER_1_STRAIGHT: { x: 437.5, y: 0.5, w: 75, h: 66 },
   
-    CAR01: { x: 160, y: 302, w: 80, h: 56 },
+    PLAYER_2_STRAIGHT: { x: 78.5, y: 316.5, w: 80, h: 59 },
   
-    SEICENTO: { x: 240, y: 302, w: 77, h: 56 },
+    POLONEZ: { x: 159.5, y: 316.5, w: 80, h: 59 },
   
-    PASIAK: { x: 265, y: 144, w: 80, h: 51 },
+    MALUCH: { x: 437.5, y: 67.5, w: 70, h: 67 },
   
-    PLAYER_UPHILL_STRAIGHT: { x: 370, y: 0, w: 80, h: 45 },
+    PLAYER_2_LEFT: { x: 240.5, y: 316.5, w: 77, h: 59 },
   
-    PLAYER_UPHILL_LEFT: { x: 370, y: 45, w: 80, h: 45 },
+    PLAYER_2_RIGHT: { x: 318.5, y: 316.5, w: 77, h: 59 },
   
-    PLAYER_UPHILL_RIGHT: { x: 370, y: 90, w: 80, h: 45 },
+    PLAYER_3_RIGHT: { x: 0.5, y: 382.5, w: 80, h: 53 },
   
-    PLAYER_RIGHT: { x: 370, y: 135, w: 80, h: 41 },
+    PLAYER_3_STRAIGHT: { x: 81.5, y: 382.5, w: 80, h: 53 },
   
-    PLAYER_STRAIGHT: { x: 370, y: 176, w: 80, h: 41 },
+    PLAYER_3_LEFT: { x: 162.5, y: 382.5, w: 80, h: 53 },
   
-    PLAYER_LEFT: { x: 370, y: 217, w: 80, h: 41 },
+    PASSAT: { x: 243.5, y: 382.5, w: 80, h: 51 },
   
-    MALUCH: { x: 370, y: 258, w: 65, h: 46 },
+    PLAYER_UPHILL_RIGHT: { x: 324.5, y: 382.5, w: 80, h: 45 },
+  
+    PLAYER_UPHILL_STRAIGHT: { x: 405.5, y: 382.5, w: 80, h: 45 },
+  
+    PLAYER_UPHILL_LEFT: { x: 0.5, y: 436.5, w: 80, h: 45 },
+  
+    OLD_PLAYER_LEFT: { x: 81.5, y: 436.5, w: 80, h: 41 },
+  
+    OLD_PLAYER_RIGHT: { x: 162.5, y: 436.5, w: 80, h: 41 },
+  
+    OLD_PLAYER_STRAIGHT: { x: 243.5, y: 436.5, w: 80, h: 41 },
   
 }
 
