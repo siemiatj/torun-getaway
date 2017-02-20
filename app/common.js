55738313a02cffb479202f0ca223e9853bc9097f
siemiatj <kuba@saucelabs.com>
2017-02-20 03:22:49 +0100

    - cleanup in common

--- app/common.js
+++ app/common.js
@@ -23,9 +23,6 @@ document.addEventListener('gesturestart', function (e) {
     e.preventDefault();
 });
 
-// Fuck you Google
-window.scrollTo(0, 1);
-
 //=========================================================================
 
 const canvas = Dom.get('canvas');
@@ -34,8 +31,6 @@ const rightTouch = Dom.get('right-touch');
 const hud = {
   speed:            { value: null, dom: Dom.get('speed_value')            },
   current_lap_time: { value: null, dom: Dom.get('current_lap_time_value') },
-  // last_lap_time:    { value: null, dom: Dom.get('last_lap_time_value')    },
-  // fast_lap_time:    { value: null, dom: Dom.get('fast_lap_time_value')    }
 }
 const orientationListener = Listener;
 
@@ -46,11 +41,6 @@ const orientationListener = Listener;
 
 const newGame = new Game({
   ...GAME_SETTINGS,
-  // canvases: {
-  //   game: canvas,
-  //   left: leftTouch,
-  //   right: rightTouch,
-  // },
   canvas,
   leftTouch,
   rightTouch,
@@ -64,25 +54,3 @@ const newGame = new Game({
 });
 
 newGame.run();
-
-//=========================================================================
-// TWEAK UI HANDLERS
-//=========================================================================
-
-// Dom.on('resolution', 'change', function(ev) {
-//   var w, h, ratio;
-//   switch(ev.target.options[ev.target.selectedIndex].value) {
-//     case 'fine':   w = 1280; h = 960;  ratio=w/width; break;
-//     case 'high':   w = 1024; h = 768;  ratio=w/width; break;
-//     case 'medium': w = 640;  h = 480;  ratio=w/width; break;
-//     case 'low':    w = 480;  h = 360;  ratio=w/width; break;
-//   }
-//   reset({ width: w, height: h })
-//   Dom.blur(ev);
-// });
-
-// Dom.on('lanes',          'change', function(ev) { Dom.blur(ev); reset({ lanes:         ev.target.options[ev.target.selectedIndex].value }); });
-
-// function refreshTweakUI() {
-//   Dom.get('lanes').selectedIndex = lanes-1;
-// }
